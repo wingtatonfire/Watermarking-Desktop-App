bc74cb408e364a24cac1d14401d75802ca0c4f95
wingtatonfire <leewingtat78@gmail.com>
2024-07-31 10:25:46 -0400

    expand the widget while the window expand

--- main.py
+++ main.py
@@ -57,67 +57,68 @@ window = tk.Tk()
 windll.shcore.SetProcessDpiAwareness(1)
 
 window.title("Watermarking")
-window.geometry("1224x768")
+window.minsize(width=1224,
+               height=768)
 padx = 40
 pady = 30
 window.option_add("*Font", "aerial 14")
-
-
+window.columnconfigure([0, 1, 2], weight=1)
+window.rowconfigure(list(range(10)), weight=1)
 # Python program to create
 # a file explorer in Tkinter
 
 label_file_explorer = tk.Label(window, text="File Selected:",
-                               width=50,
-                               height=8,
+                               height=8, width=55, anchor="w", justify="left"
                                )
-label_file_explorer.grid(column=1, row=1, padx=padx, pady=pady)
+label_file_explorer.grid(column=1, row=1, padx=padx, pady=pady, sticky="nsew")
 
 
-button_explore = tk.Button(window, text="Browse Files", command=lambda: browse_files())
-button_explore.grid(column=2, row=1, padx=padx, pady=pady)
+button_explore = tk.Button(window, text="Browse Files", command=lambda: browse_files(), width=16)
+button_explore.grid(column=2, row=1, padx=padx, pady=pady, sticky="w")
 
 
 entry_for_watermark_text = tk.Entry(window)
-entry_for_watermark_text.grid(column=1, row=3, padx=padx, pady=pady)
+entry_for_watermark_text.grid(column=1, row=3, padx=padx, pady=pady, sticky="ew")
 
 
 button_add_mark = tk.Button(window,
                             text="Add Watermark",
                             command=lambda: watermark_adding(filenames, entry_for_watermark_text.get(),
                                                              int(opacity_scale.get() * 2.25),
-                                                             filepath=path)
+                                                             filepath=path),
+                            width=16
                             )
-button_add_mark.grid(column=1, row=9, padx=padx, pady=pady)
+button_add_mark.grid(column=2, row=8, padx=padx, pady=pady, sticky="w")
 
 opacity_scale = tk.Scale(window, from_=0, to=100, orient="horizontal", length=200)
-opacity_scale.grid(column=1, row=4, padx=padx, pady=pady)
+opacity_scale.grid(column=1, row=4, padx=padx, pady=pady, sticky="w")
 
-button_for_directory = tk.Button(window, text="Choose", command=ask_directory)
-button_for_directory.grid(column=2, row=5, padx=padx, pady=pady)
+button_for_directory = tk.Button(window, text="Choose", command=ask_directory, width=16)
+button_for_directory.grid(column=2, row=5, padx=padx, pady=pady, sticky="w")
 
 label_for_directory = tk.Label(window, text="Filepath Selected: ")
-label_for_directory.grid(column=1, row=5, padx=padx, pady=pady)
+label_for_directory.grid(column=1, row=5, padx=padx, pady=pady, sticky="w")
 
 entry_for_filename = tk.Entry(window)
-entry_for_filename.grid(column=1, row=8, padx=padx, pady=pady)
+entry_for_filename.grid(column=1, row=8, padx=padx, pady=pady, sticky="ew")
 
 # image_no_1 = ImageTk.PhotoImage(Image.open("5.png"))
 # label = tk.Label(image=image_no_1)
 
 upload_label = tk.Label(window, text="Upload Photo")
-upload_label.grid(column=0, row=1, padx=padx, pady=pady)
+upload_label.grid(column=0, row=1, padx=padx, pady=pady, sticky="w")
 
 label_for_watermark_text = tk.Label(window, text="Word for Watermark")
-label_for_watermark_text.grid(column=0, row=3, padx=padx, pady=pady)
+label_for_watermark_text.grid(column=0, row=3, padx=padx, pady=pady, sticky="w")
 
 label_for_opacity = tk.Label(window, text="Opacity")
-label_for_opacity.grid(column=0, row=4, padx=padx, pady=pady)
+label_for_opacity.grid(column=0, row=4, padx=padx, pady=pady, sticky="w")
 
 label_for_filepath = tk.Label(window, text="Filepath")
-label_for_filepath.grid(column=0, row=5, padx=padx, pady=pady)
+label_for_filepath.grid(column=0, row=5, padx=padx, pady=pady, sticky="w")
 
 label_for_filename = tk.Label(window, text="Filename")
-label_for_filename.grid(column=0, row=8, padx=padx, pady=pady)
+label_for_filename.grid(column=0, row=8, padx=padx, pady=pady, sticky="w")
 
 window.mainloop()
 
